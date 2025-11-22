317eb390e2f24bab328254464cc28f204cff78e2
kojikjazu0525-cell <kojikjazu0525@gmail.com>
2025-11-22 22:16:41 +0000

    🔧 品質改善: 2025-11-22 22:16

--- data_models/__init__.py
+++ data_models/__init__.py
@@ -1,11 +1,7 @@
-﻿# data_models/__init__.py
+# data_models/__init__.py
 """データモデルパッケージ"""
-from .data_models import (
-    ErrorSeverity,
-    ErrorCategory,
-    ErrorContextModel,
-    BugFixTask,
-    FixResult
-)
-__all__ = ['ErrorSeverity', 'ErrorCategory', 'ErrorContextModel', 'BugFixTask', 'FixResult']
-__version__ = '1.0.0'
+from .data_models import (BugFixTask, ErrorCategory, ErrorContextModel,
+                          ErrorSeverity, FixResult)
+
+__all__ = ["ErrorSeverity", "ErrorCategory", "ErrorContextModel", "BugFixTask", "FixResult"]
+__version__ = "1.0.0"
